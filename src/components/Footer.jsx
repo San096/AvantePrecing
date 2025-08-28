01cd35aca1b1d371fc3164127c7086e49d32e7f5
San096 <sanderleycc2022@alu.ufc.br>
2025-08-28 17:49:31 -0300

    correção do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="w-full bg-blue-200 text-center text-xs py-3 mt-10 flex flex-col items-center gap-2">
       <img src={logo} alt="Logo ÁSAP-DEV" className="h-8" />
       <p>
-        Produzido por <span className="font-semibold">ÁSAP-DEV</span>
+        Produzido por <span className="font-semibold">SASP-DEV</span>
       </p>
     </footer>
   );
